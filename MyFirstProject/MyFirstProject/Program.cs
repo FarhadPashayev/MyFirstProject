5f3c57e8a172e3e508a3b6cffe4c907937bb834b
farhad <farhadip@code.edu.az>
2020-11-24 09:56:39 +0400

    Update Program.cs,

--- MyFirstProject/MyFirstProject/Program.cs
+++ MyFirstProject/MyFirstProject/Program.cs
@@ -34,7 +34,7 @@ namespace MyFirstProject
                     case 0:
                         continue;
                    case 1:
-                        ShowProduct();
+                        ShowProductsMenu();
                         break;
                     case 2:
                         ShowSale();
@@ -51,30 +51,128 @@ namespace MyFirstProject
                 
 
             } while (selectInt != 0);
-
-            static void ShowProduct()
+           
+            static void ShowProductsMenu()
+            
+                #region second Menu 
             {
-                Console.WriteLine("---------------Məhsullar üzərində əməliyyat aparmaq--------------");
-                Console.WriteLine("1. Yeni məhsul əlavə et ");
-                Console.WriteLine("2. Məhsul üzərində düzəliş et ");
-                Console.WriteLine("3. Məhsulu sil");
-                Console.WriteLine("4. Bütün məhsulları göstər ");
-                Console.WriteLine("5. Kateqoriyasına görə məhsulları göstər");
-                Console.WriteLine("6. Qiymət aralığına görə məhsulları göstər ");
-                Console.WriteLine("7. Məhsullar arasında ada görə axtarış et ");
+                    int SelectInt1;
+                    do
+                    {
+                    Console.WriteLine("---------------Məhsullar üzərində əməliyyat aparmaq--------------");
+                    Console.WriteLine("1. Yeni məhsul əlavə et ");
+                    Console.WriteLine("2. Məhsul üzərində düzəliş et ");
+                    Console.WriteLine("3. Məhsulu sil");
+                    Console.WriteLine("4. Bütün məhsulları göstər ");
+                    Console.WriteLine("5. Kateqoriyasına görə məhsulları göstər");
+                    Console.WriteLine("6. Qiymət aralığına görə məhsulları göstər ");
+                    Console.WriteLine("7. Məhsullar arasında ada görə axtarış et ");
+                    #endregion
+                    #region Second Menu Selections
+
+                    string Select = Console.ReadLine();
+                    while (!int.TryParse(Select, out SelectInt1))
+                    {
+                    Console.WriteLine("Rəqəm daxil etməlisiniz");
+                    Select = Console.ReadLine();
+                     }
+
+
+                    #endregion
+
+                    #region second Menu Switch
+                    switch (SelectInt1)
+                    {
+                        case 1:
+                            continue;
+                        case 2:
+                            continue;
+                        case 3:
+                            continue;
+                        case 4:
+                            continue;
+                        case 5:
+                            continue;
+                        case 6:
+                            continue;
+                        case 7:
+                            continue;                         
+                        default:
+                            Console.WriteLine("------------------------------------------");
+                            Console.WriteLine("siz yalnış seçim etdiniz,1-7 arasında seçim edin ");
+                            Console.WriteLine("----------------------------------------------------");
+                            break;
+                    }
+
+
+
+                    #endregion
+
+
+                } while (SelectInt1 != 0);
+
             }
+             
+           
             static void  ShowSale()
+            #region Three  Menu 
             {
-                Console.WriteLine("--------------Satışlar üzərində əməliyyat aparmaq----------------");
-                Console.WriteLine("1. Yeni satış əlavə etmək ");
-                Console.WriteLine("2. Satışdan hansısa məhsulun geri qaytarılması");
-                Console.WriteLine("3. Satışın silinməsi ");
-                Console.WriteLine("4. Bütün satışların ekrana çıxarılması");
-                Console.WriteLine("5. Verilən tarix aralığına görə satışların göstərilməsi ");
-                Console.WriteLine("6. Verilən məbləğ aralığına görə satışların göstərilməsi "); 
-                Console.WriteLine("7. Verilmiş bir tarixdə olan satışların göstərilməsi"); 
-                Console.WriteLine("8. Verilmiş nömrəyə əsasən həmin nömrəli satışın məlumatlarının göstərilməsi ");
+                int SelectInt2;
+                do
+                {
 
+                    Console.WriteLine("--------------Satışlar üzərində əməliyyat aparmaq----------------");
+                    Console.WriteLine("1. Yeni satış əlavə etmək ");
+                    Console.WriteLine("2. Satışdan hansısa məhsulun geri qaytarılması");
+                    Console.WriteLine("3. Satışın silinməsi ");
+                    Console.WriteLine("4. Bütün satışların ekrana çıxarılması");
+                    Console.WriteLine("5. Verilən tarix aralığına görə satışların göstərilməsi ");
+                    Console.WriteLine("6. Verilən məbləğ aralığına görə satışların göstərilməsi ");
+                    Console.WriteLine("7. Verilmiş bir tarixdə olan satışların göstərilməsi");
+                    Console.WriteLine("8. Verilmiş nömrəyə əsasən həmin nömrəli satışın məlumatlarının göstərilməsi ");
+                    #endregion
+                    #region Three Menu Selections
+                    string Select = Console.ReadLine();
+                    while (!int.TryParse(Select, out SelectInt2))
+                    {
+                        Console.WriteLine("Rəqəm daxil etməlisiniz");
+                        Select = Console.ReadLine();
+                    }
+
+                    #endregion
+                    #region Three Menu Switch
+                    switch (SelectInt2)
+                    {
+                        case 1:
+                            continue;
+                        case 2:
+                            continue;
+                        case 3:
+                            continue;
+                        case 4:
+                            continue;
+                        case 5:
+                            continue;
+                        case 6:
+                            continue;
+                        case 7:
+                            continue;
+                        case 8:
+                            continue;
+                        default:
+                            Console.WriteLine("------------------------------------------");
+                            Console.WriteLine("siz yalnış seçim etdiniz,1-8 arasında seçim edin ");
+                            Console.WriteLine("----------------------------------------------------");
+                            break;
+                    }
+
+
+                    #endregion
+
+
+                } while (SelectInt2 != 0);
+
+               
             }
         }
     }
